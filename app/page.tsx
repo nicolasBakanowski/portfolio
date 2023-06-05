ca4b39bea02a9d7a62c7e3d52b4f4987cf44245c
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-06-05 06:22:16 -0300

    experience section

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import Head from "next/head"
 import Header from "../components/Header"
 import Hero from "../components/Hero"
 import About from "../components/About"
+import Experience from "../components/Experience"
+
 export default function Home() {
- 
   return (
-  <div className="bg-[rgb(36,36,36)] text-yellow-50 h-screen snap-y snap-mandatory overflow-scroll z-0">
+  <div className=" bg-[rgb(36,36,36)] text-yellow-50 h-screen snap-y snap-mandatory overflow-scroll z-0 scroll-behavior">
     <Head>
       <title>Nico Portfolio</title>
     </Head>
@@ -15,6 +16,9 @@ export default function Home() {
     </section>
     <section id='about' className="snap-center">
       <About />
+    </section>
+    <section id='experience' className="snap-center">
+      <Experience/>
     </section>
   </div>
   )
