a3bae8246a6a21bd00a9cf33befeda75053e08bf
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-06-07 06:56:31 -0300

    fix animation hover in skils

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -24,9 +24,21 @@ const Skill = ({ directionLeft, url }: Props) => {
         className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out
          group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0"
       >
-        <div className="flex items-center justify-center h-full">
-          <p className="text-3xl font-bold text-black opacity-100">100%</p>
-        </div>
+        <motion.div
+          initial={{ x: directionLeft ? -200 : 200 }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          className="flex items-center justify-center h-full"
+        >
+          <motion.p
+            initial={{ x: directionLeft ? -200 : 200 }}
+            transition={{ duration: 1 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            className="text-3xl font-bold text-black opacity-100"
+          >
+            100%
+          </motion.p>
+        </motion.div>
       </div>
     </div>
   )
